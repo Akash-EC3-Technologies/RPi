e18ef5b178aee818f997dfe81f1d9f7cd7d4e7b9
AkashSelvaraju <akash.selvaraju2000@gmail.com>
2025-08-29 01:31:33 +0530

    update gpio lib

--- CAN/TailLamp/hal/hal_tail_lamp.c
+++ CAN/TailLamp/hal/hal_tail_lamp.c
@@ -1,26 +1,24 @@
 #include "hal_tail_lamp.h"
 #include "../lib/gpio/gpio.h"
 
-static int g_tail_lamp_pin = 17;
+static struct gpio_handle tail_lamp_handle;
 
 void hal_tail_lamp_init(int pin)
 {
-    g_tail_lamp_pin = pin;
-    gpio_init(g_tail_lamp_pin);
+    gpio_init(&tail_lamp_handle, "/dev/gpiochip0", pin, GPIO_OUTPUT);
 }
 
 void hal_tail_lamp_set_state(tail_lamp_state_t state)
 {
-    gpio_set(g_tail_lamp_pin, state);
+    gpio_set(&tail_lamp_handle, state);
 }
 
 tail_lamp_state_t hal_tail_lamp_get_state(void)
 {
-    return gpio_read(g_tail_lamp_pin);
+    return gpio_get(&tail_lamp_handle);
 }
 
 void hal_tail_lamp_cleanup()
 {
-    gpio_cleanup(g_tail_lamp_pin);
-    g_tail_lamp_pin = 0;
+    gpio_cleanup(&tail_lamp_handle);
 }
--- CAN/TailLamp/lib/gpio/gpio_mock.c
+++ CAN/TailLamp/lib/gpio/gpio_mock.c
@@ -1,24 +1,47 @@
 #include "gpio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-static int pins[40];
-
-void gpio_init(int pin)
+typedef struct
 {
-    // init given pin as 0
-    pins[pin] = 0;
+    int value;
+} mock_internal;
+
+int gpio_init(gpio_handle *handle, const char *chipname, unsigned int line_num, gpio_direction dir)
+{
+    printf("[MOCK] Init GPIO %u as %s\n", line_num, dir == GPIO_OUTPUT ? "OUTPUT" : "INPUT");
+    mock_internal *internal = malloc(sizeof(mock_internal));
+    if (!internal)
+        return -1;
+    internal->value = 0;
+    handle->internal = internal;
+    handle->direction = dir;
+    handle->line_num = line_num;
+    return 0;
 }
 
-void gpio_set(int pin, int value)
+int gpio_set(gpio_handle *handle, int value)
 {
-    pins[pin] = value;
+    if (handle->direction != GPIO_OUTPUT)
+        return -1;
+    mock_internal *internal = handle->internal;
+    internal->value = value;
+    printf("[MOCK] Set GPIO %u to %d\n", handle->line_num, value);
+    return 0;
 }
 
-int gpio_read(int pin)
+int gpio_get(gpio_handle *handle)
 {
-    return pins[pin];
+    if (handle->direction != GPIO_INPUT)
+        return -1;
+    mock_internal *internal = handle->internal;
+    printf("[MOCK] Read GPIO %u -> %d\n", handle->line_num, internal->value);
+    return internal->value;
 }
 
-void gpio_cleanup(int pin)
+void gpio_cleanup(gpio_handle *handle)
 {
-    pins[pin] = 0;
+    printf("[MOCK] Cleanup GPIO %u\n", handle->line_num);
+    free(handle->internal);
+    handle->internal = NULL;
 }
